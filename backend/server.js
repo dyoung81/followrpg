495ba821a7fa424e94c4cb311292bc5d3f6a3680
Daniel Young <dyou6376@gmail.com>
2021-01-30 23:48:27 -0500

    added server package

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose")
 const routes = require("./routes")
+
 mongoose
 	.connect(process.env.DB_CONNECT,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 	.then(() => {
         app.use(cors())
         app.use(express.json())
 		app.use("/api", routes)
-		app.listen(5000, () => {
-			console.log("Server has started!")
+		app.listen(PORT, () => {
+			console.log(`Server has started at ${PORT}!`)
 		})
 	})
