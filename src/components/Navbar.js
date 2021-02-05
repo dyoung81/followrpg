2bf858e66bbfe7f3dfca0dd1299c30044ff89cd9
Daniel Young <dyou6376@gmail.com>
2021-02-04 22:43:24 -0500

    Corrected makeStyles issue in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,9 @@
 
 import Logo from '../media/logo.png'
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import TemporaryDrawer from './Drawer';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
 const TheLogo = () => <img src={Logo} height='45' alt="" />
