b93c099545c9d50d1feba3063c349d2b619d2386
Daniel Young <dyou6376@gmail.com>
2021-02-08 22:14:48 -0500

    testing new routing in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoose = require("mongoose")
-const routes = require("./routes")
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('../build'))
+if (process.env.NODE_ENV === "production") {
+  //app.use(express.static("../build"));
+  app.get("/*", (req, res) => {
+    let url = path.join(__dirname, "../build", "index.html");
+    if (!url.startsWith("/app/"))
+      // we're on local windows
+      url = url.substring(1);
+    res.sendFile(url);
+  });
 }
 
 mongoose
-	.connect(process.env.MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-	.then(() => {
-        app.use(cors())
-        app.use(express.json())
-		app.use("/api", routes)
-		app.listen(PORT, () => {
-			console.log(`Server has started at ${PORT}!`)
-		})
-	})
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    app.use(cors());
+    app.use(express.json());
+    app.use("/api", routes);
+    app.listen(PORT, () => {
+      console.log(`Server has started at ${PORT}!`);
+    });
+  });
