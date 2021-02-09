22be8b8838c460ac51c36b9972d267a641afa29b
Daniel Young <dyou6376@gmail.com>
2021-02-08 21:50:48 -0500

    Added switch to router

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -3,18 +3,20 @@ import { useStyles } from "../CustomHooks";
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import GameDetails from "../pages/GameDetails";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function AppBody(props) {
   const classes = useStyles();
 
   return (
     <Router>
-      <div>
-        <Route path="/" exact component={Home} />
-        <Route path="/search" exact component={Search} />
-        <Route path="/gamedetails" exact component={GameDetails} />
-      </div>
+      <Switch>
+        <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" exact component={Search} />
+          <Route path="/gamedetails" exact component={GameDetails} />
+        </div>
+      </Switch>
     </Router>
   );
 }
