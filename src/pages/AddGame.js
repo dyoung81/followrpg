28a0f46e7413fd3d638633494ee53004e7f97bb1
Daniel Young <dyou6376@gmail.com>
2021-02-13 12:57:56 -0500

    Backend converted to its own repository

--- src/pages/AddGame.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AddGame(props) {
-  return (
-    <div>
-      <h1>Add Game</h1>
-    </div>
-  );
-}
--- src/pages/Search.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Search(props) {
-  return (
-    <div>
-      <h1>Search</h1>
-    </div>
-  );
-}
