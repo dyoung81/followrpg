8ff22a47d284752e9eade5bb9c4660f2bed057af
Daniel Young <dyou6376@gmail.com>
2021-02-17 13:59:19 -0500

    fixed cookies with rollover

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const passport = require("passport");
 const User = require("./models/User");
 const router = require("./routes");
@@ -27,16 +28,21 @@ app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 var sess = {
-  secret: "secretcode",
+  secret: process.env.SESS_SECRET,
   resave: false,
   saveUninitialized: false,
+  rolling: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    touchAfter: 60 * 4,
+  }),
   cookie: {
-    maxAge: 1000 * 60 * 5,
+    maxAge: 1000 * 60 * 20,
   },
 };
 if (process.env.NODE_ENV == "production") {
-  app.set("trust proxy", 1); // trust first proxy
-  sess.cookie.secure = true; // serve secure cookies
+  app.set("trust proxy", 1);
+  sess.cookie.secure = true;
 }
 
 app.use(session(sess));
@@ -48,7 +54,6 @@ passport.serializeUser((user, done) => {
 });
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, doc) => {
-    console.log(doc);
     // Whatever we return goes to the client and binds to the req.user property
     return done(null, doc);
   });
@@ -59,7 +64,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: proccess.env.GOOGLE_CALLBACK,
     },
     function (_, __, profile, cb) {
       User.findOne({ googleId: profile.id }, async (err, doc) => {
